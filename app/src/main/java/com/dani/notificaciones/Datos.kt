c03e5e41f90d51bc31f65da941234467634c1755
Dani Martínez <danimartinezsaa@gmail.com>
2019-02-19 13:42:08 +0100

    Implementado formulario para enviar pedido a un bar con base de datos en tiempo real firebase

--- app/src/main/java/com/dani/notificaciones/Datos.kt
+++ app/src/main/java/com/dani/notificaciones/Datos.kt
@@ -2,15 +2,19 @@ package com.dani.notificaciones
 
 import java.util.*
 
-data class Datos(var token: String, var nombreDispositivo: String, var hora: Date){
+data class Datos(var cafeleche: String, var cafesololargo: String, var croissant: String, var tostada: String, var tortilla: String,var confirmado: Boolean){
 
     //Lista para almacenar los Datos
     val miHashMapDatos = HashMap<String, Any>()
 
     //Rellenamos el HashMap
     fun crearHashMapDatos() {
-        miHashMapDatos.put("token", token)
-        miHashMapDatos.put("nombre", nombreDispositivo)
-        miHashMapDatos.put("hora", hora)
+        miHashMapDatos.put("cafeleche", cafeleche)
+        miHashMapDatos.put("cafesololargo", cafesololargo)
+        miHashMapDatos.put("croissant", croissant)
+        miHashMapDatos.put("tostada", tostada)
+        miHashMapDatos.put("tortilla", tortilla)
+        miHashMapDatos.put("confirmado",confirmado)
+
     }
 }
--- app/src/main/java/com/dani/notificaciones/MainActivity.kt
+++ app/src/main/java/com/dani/notificaciones/MainActivity.kt
@@ -11,17 +11,15 @@ import com.google.firebase.iid.FirebaseInstanceId
 
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
+import org.jetbrains.anko.toast
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     //Objeto para referenciar Database
     private var database: DatabaseReference? = null
-    // Token del dispositivo
-    private var token: String? = null
-    // key unica creada automaticamente al añadir un child
     lateinit var misDatos : Datos
-    lateinit var key: String
+    lateinit var usuario: String
     // para actualizar los datos necesito un hash map
     val miHashMapChild = HashMap<String, Any>()
 
@@ -29,39 +27,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+        // referencia a la base de datos del proyecto en firebase
+        database = FirebaseDatabase.getInstance().getReference("/dispositivos")
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Tiempo actualizado", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-            // cada vez que le damos click actualizamos tiempo
-            misDatos.hora = Date()
-            // Creamos el hashMap en el objeto
-            misDatos.crearHashMapDatos()
-            // actualizamos la base de datosw
-            miHashMapChild.put(key.toString(),misDatos.miHashMapDatos)
-            // actualizamos el child
-            database!!.updateChildren(miHashMapChild)
-        }
+            if(innombre.text!=null){
+                Snackbar.make(view, "Pedido enviado", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+                misDatos = Datos("","","","","",false)
 
-        //Para que no se ejecute al girar
-        if (savedInstanceState == null) {
-            try {
-                //Obtengo el token del dispositivo.
-                token = FirebaseInstanceId.getInstance().token
-                //creamos una entrada nueva en el child "dispositivos" con un key unico automatico
-                key = database!!.push().key!!
-                //guardamos el token, dispositivo, tiempo actual en la data class
-                misDatos = Datos(token.toString(),android.os.Build.MANUFACTURER+" "+android.os.Build.ID,Date())
-                //creamos el hash map
+                misDatos.cafeleche = incafeleche.text.toString()
+                misDatos.cafesololargo = incafesololargo.text.toString()
+                misDatos.croissant = incroissant.text.toString()
+                misDatos.tostada = intostada.text.toString()
+                misDatos.tortilla = intortilla.text.toString()
+                misDatos.confirmado = false
+
+                // Creamos el hashMap en el objeto
                 misDatos.crearHashMapDatos()
-                //guardamos los datos en el hash map para la key creada anteriormente
-                miHashMapChild.put(key.toString(), misDatos.miHashMapDatos)
-                //actualizamos el child
+                usuario=innombre.text.toString()
+                // actualizamos la base de datos
+                miHashMapChild.put(usuario,misDatos.miHashMapDatos)
+                // actualizamos el child
                 database!!.updateChildren(miHashMapChild)
-            } catch (e: Exception) {
-                e.printStackTrace()
-                Log.d("Notificaciones","Error escribiendo datos ${e}")
+            }else{
+                toast("Inserte Nombre para el pedido")
             }
+
         }
 
         initListener()
@@ -74,25 +66,25 @@ class MainActivity : AppCompatActivity() {
     fun initListener() {
         val childEventListener = object : ChildEventListener {
             override fun onChildRemoved(p0: DataSnapshot) {
-                Log.d("Notificaciones", "Datos borrados: " + p0.key)
+                Log.d("Bar", "Datos borrados")
             }
-
             override fun onChildChanged(p0: DataSnapshot, p1: String?) {
-                Log.d("Notificaciones", "Datos cambiados: " + (p0.getValue() as HashMap<*,*>).toString())
-                val has=(p0.child("hora").getValue() as HashMap<*,*>)
-                txthora.text=has["hours"].toString()+" : "+has["minutes"].toString()+" : "+has["seconds"].toString()
+                Log.d("Bar", "Datos cambiados: " + (p0.getValue() as HashMap<*,*>).toString())
+                val lista=(p0.getValue() as HashMap<*,*>)
+                val confirmado=lista["confirmado"].toString()
+                if(confirmado.equals("true")){
+                    checkConfirmado.setChecked(true)
+                }else{
+                    checkConfirmado.setChecked(false)
+                }
             }
-
             override fun onChildMoved(p0: DataSnapshot, p1: String?) {
                 Log.d("Notificaciones", "Datos movidos")
             }
-
             override fun onChildAdded(p0: DataSnapshot, p1: String?) {
                 // onChildAdded() capturamos la key
-                Log.d("Notificaciones", "Datos añadidos: " + p0.key)
-
+                Log.d("Notificaciones", "Datos añadidos.")
             }
-
             override fun onCancelled(p0: DatabaseError) {
                 Log.d("Notificaciones", "Error cancelacion")
             }
